644bc2be060e33120879471a85c7780dba69100c
ritesh <ritesh.nisanka@gmail.com>
2022-09-05 10:02:25 -0700

    logger and exception completed

--- app.py
+++ app.py
@@ -1,9 +1,18 @@
 from flask import Flask
+import sys
+from housing.logger import logging
+from housing.exception import HousingException
 
 app = Flask(__name__)
 
 @app.route('/',methods = ['GET','POST'])
 def index():
+    try:
+        raise Exception("we are testing exception")
+    except Exception as e:
+        housing = HousingException(e,sys)
+        logging.info(housing.error_message)
+        logging.info("we are testing a logging module")
     return 'Starting Machine Learning Project'
 
 if __name__ == "__main__":
